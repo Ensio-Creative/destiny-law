89574c7ea3367737766698eb5ce0e2420aaed2f9
greatAdams01 <kingifean@gmail.com>
2022-06-02 22:40:04 +0100

    Mobile modifications

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,12 +2,13 @@
 const props = defineProps({
   title: String,
   img: String,
-  text: String
+  text: String,
+  mx: String
 })
 </script>
 
 <template>
-  <div class="bg-white rounded-2xl p-8 shadow md:w-[345px]">
+  <div class="bg-white rounded-2xl p-8 shadow md:w-[345px]" :class="mx">
     <img class="w-[60px] py-6" :src="`/img/${props.img}`" alt="">
     <h5 class="reco text-[23px] pb-4 font-semibold">{{ props.title }}</h5>
     <p class="font-inter text-[16px] pb-5">
